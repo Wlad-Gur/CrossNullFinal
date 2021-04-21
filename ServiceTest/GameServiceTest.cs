b523b50dba3932512bfa19f8b2b67a0b6c124d8d
Vlad <Vlad@VLAD-LAPTOP>
2021-04-21 23:46:36 +0300

    +test :P couldn't write prop State

---
+++ ServiceTest/GameServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using CrossNull.Domain;
+using CrossNull.Logic.Models;
+using CrossNull.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ServiceTest
+{
+    [TestClass]
+    public class GameServiceTest
+    {
+        IEnumerable<Cell> cells;
+
+        Player _playerOne = new Player(PlayerTypes.X, "One");
+        GameState gameState = new GameState();
+        GameModel _gameModel = new GameModel()
+        {
+            Id = 1,
+            PlayerOne = new Player(PlayerTypes.X, "One"),
+            PlayerTwo = new Player(PlayerTypes.O, "Two"),
+            PlayerActive = new Player(PlayerTypes.X, "One"),
+            gameState,
+        };
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
